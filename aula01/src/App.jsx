c203bbff5af1b331289415dced273f186ea21b0b
Guilherme Henrique de Souza <guilhermehsouza16@gmail.com>
2026-08-05 10:18:17 -0300

    05/08 - 2vez

--- aula01/src/App.jsx
+++ aula01/src/App.jsx
@@ -1,11 +1,17 @@
 export default function App()
 {
+  let nome, idade, altura;
+
+  nome = "Guilherme";
+
   return (
     <div>
 
       <h1 style={{color : "red"}}>Hello, World!</h1>
 
-
+      <p>
+        Olá {nome}, seja bem-vindo ao JavaScript!
+      </p>
     </div>
   );
 }
